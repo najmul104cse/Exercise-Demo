d7749f6266fa119801e25f6b8c6180cecc8ee43e
najmul104cse <najmulovi999@gmail.com>
2019-03-28 19:03:33 +0600

    Added a new method called greeting

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String [] args){
         System.out.println("I am back again to learning Github by push this code...")
+        greeting("Najmul Ovi");
     }
+
+    public static void greeting(String name){
+
+        System.out.println("Welcome" + name);
+    } 
 }
